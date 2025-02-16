a84440d38ebca2d6952a99f92dd4d1e3e14ca446
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-16 18:34:04 -0300

    Update Main.java

--- Trabalho Final/src/Main.java
+++ Trabalho Final/src/Main.java
@@ -15,5 +15,38 @@ public class Main {
         System.out.println(usuario1.getSenha());
 
         System.out.println();
+
+     Product produto1 = new Product("Notebook Dell","Notebook Dell Inspiron 15, 8GB RAM, 256GB SSD",
+             3.500f,10,"imagens/notebook_dell.jpg", false);
+
+        System.out.println(produto1.getNome());
+        System.out.println(produto1.getDescricao());
+        System.out.println(String.format("%.3f", produto1.getPrecoVenda()));
+        System.out.println(produto1.getEstoque());
+        System.out.println(produto1.getCaminhoImagem());
+        System.out.println(produto1.isUsado());
+
+        cadastros.CadastroProduto(produto1);
+
+        FormasPagamento forma1 = new FormasPagamento(1,"PIX", 3, 15.0f);
+
+        cadastros.CadastraFormaPagamento(forma1);
+
+        System.out.println();
+
+        System.out.println(forma1.getForma());
+        System.out.println(forma1.getPercJuros());
+        System.out.println(forma1.getQuantParcelas());
+
+        Vendas venda1 = new Vendas(usuario1, produto1, 1,  forma1);
+
+        System.out.println();
+
+        cadastros.CadastraVenda(venda1);
+
+        System.out.println(usuario1.getUsername());
+        System.out.println(produto1.getNome());
+        System.out.println(forma1.getQuantParcelas());
+        System.out.println(forma1.getPercJuros());
     }
 }
