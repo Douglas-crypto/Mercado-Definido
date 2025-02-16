d9ababcd05f47bc1fe39a9ac05fdb7097543763d
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-16 14:22:20 -0300

    Update Main.java

--- Trabalho Final/src/Main.java
+++ Trabalho Final/src/Main.java
@@ -15,18 +15,5 @@ public class Main {
         System.out.println(usuario1.getSenha());
 
         System.out.println();
-
-        User usuario2 = new User("Maria Oliveira","23456789012",
-                "Rua B 456","maria.oliveira@email.com","senha456");
-
-        cadastros.CadastroUser(usuario2);
-
-        System.out.println(usuario2.getUsername());
-        System.out.println(usuario2.getCpf_cnpj());
-        System.out.println(usuario2.getEndereco());
-        System.out.println(usuario2.getEmail());
-        System.out.println(usuario2.getSenha());
-
-        System.out.println();
     }
 }
