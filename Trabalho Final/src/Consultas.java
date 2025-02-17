03edd1835a38b904b4a3f6333a59716cdd63e9f5
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-17 19:43:18 -0300

    Update Consultas.java

--- Trabalho Final/src/Consultas.java
+++ Trabalho Final/src/Consultas.java
@@ -1,6 +1,35 @@
 public class Consultas {
+    Cadastros cadastros;
 
-    public void ConsultaUsuario(){
 
+    public Consultas(Cadastros cadastros){
+        this.cadastros = cadastros;
+    }
+
+   public User consultaUsuarioPorId(int id){
+       for (User user : cadastros.usuarios){
+           if (user.getId() == id){
+               return user;
+       }
+   }
+       return null;
+    }
+
+    public Product consultaProdutoID(int id){
+        for (Product product : cadastros.produtos){
+            if (product.getId() == id){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void consultaGeral() {
+        for (Vendas venda : cadastros.vendas) {
+            System.out.println("Usuário ID: " + venda.getUsuario().getId() +
+                    ", Produto ID: " + venda.getProduto().getId() +
+                    ", Quantidade: " + venda.getQuantidade() +
+                    ", Valor: " + venda.getProduto().getPrecoVenda());
+        }
     }
 }
