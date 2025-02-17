757d4f49caf6c8af66e11133f94e7a30a40ade72
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-17 19:53:34 -0300

    Update Consultas.java

--- Trabalho Final/src/Consultas.java
+++ Trabalho Final/src/Consultas.java
@@ -2,22 +2,22 @@ public class Consultas {
     Cadastros cadastros;
 
 
-    public Consultas(Cadastros cadastros){
+    public Consultas(Cadastros cadastros) {
         this.cadastros = cadastros;
     }
 
-   public User consultaUsuarioPorId(int id){
-       for (User user : cadastros.usuarios){
-           if (user.getId() == id){
-               return user;
-       }
-   }
-       return null;
+    public User consultaUsuarioPorId(int id) {
+        for (User user : cadastros.usuarios) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
     }
 
-    public Product consultaProdutoID(int id){
-        for (Product product : cadastros.produtos){
-            if (product.getId() == id){
+    public Product consultaProdutoID(int id) {
+        for (Product product : cadastros.produtos) {
+            if (product.getId() == id) {
                 return product;
             }
         }
@@ -30,6 +30,18 @@ public class Consultas {
                     ", Produto ID: " + venda.getProduto().getId() +
                     ", Quantidade: " + venda.getQuantidade() +
                     ", Valor: " + venda.getProduto().getPrecoVenda());
+
+            
+            for (AvaliaCao avaliacao : cadastros.avaliacao) {
+                if (avaliacao.getUsuario().getId() == venda.getUsuario().getId() &&
+                        avaliacao.getProduto().getId() == venda.getProduto().getId()) {
+
+                    System.out.println("  -> Avaliação ID: " + avaliacao.getId() +
+                            ", Rating: " + avaliacao.getRating() +
+                            ", Comentário: " + avaliacao.getTexto());
+                    break; 
+                }
+            }
         }
     }
 }
