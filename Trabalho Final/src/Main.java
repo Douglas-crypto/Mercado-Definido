099d9a1d0d88a20dbb263656924893bd52a4067b
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-17 19:42:45 -0300

    Update Main.java

--- Trabalho Final/src/Main.java
+++ Trabalho Final/src/Main.java
@@ -3,50 +3,46 @@ public class Main {
 
         Cadastros cadastros = new Cadastros();
 
-        User usuario1 = new User("João Silva", "12345678901",
+        User usuario1 = new User(1,"João Silva", "12345678901",
                 "Rua A 123", "joao.silva@email.com", "senha123");
 
         cadastros.CadastroUser(usuario1);
 
-        System.out.println(usuario1.getUsername());
-        System.out.println(usuario1.getCpf_cnpj());
-        System.out.println(usuario1.getEndereco());
-        System.out.println(usuario1.getEmail());
-        System.out.println(usuario1.getSenha());
-
         System.out.println();
 
-     Product produto1 = new Product("Notebook Dell","Notebook Dell Inspiron 15, 8GB RAM, 256GB SSD",
+     Product produto1 = new Product(1,"Notebook Dell","Notebook Dell Inspiron 15, 8GB RAM, 256GB SSD",
              3.500f,10,"imagens/notebook_dell.jpg", false);
-
-        System.out.println(produto1.getNome());
-        System.out.println(produto1.getDescricao());
-        System.out.println(String.format("%.3f", produto1.getPrecoVenda()));
-        System.out.println(produto1.getEstoque());
-        System.out.println(produto1.getCaminhoImagem());
-        System.out.println(produto1.isUsado());
-
         cadastros.CadastroProduto(produto1);
 
-        FormasPagamento forma1 = new FormasPagamento(1,"PIX", 3, 15.0f);
-
+        FormasPagamento forma1 = new FormasPagamento(1,"PIX",3,15.0f);
         cadastros.CadastraFormaPagamento(forma1);
 
         System.out.println();
 
-        System.out.println(forma1.getForma());
-        System.out.println(forma1.getPercJuros());
-        System.out.println(forma1.getQuantParcelas());
-
-        Vendas venda1 = new Vendas(usuario1, produto1, 1,  forma1);
+        Vendas venda1 = new Vendas(1,usuario1, produto1, 1,  forma1);
+        cadastros.CadastraVenda(venda1);
 
         System.out.println();
 
-        cadastros.CadastraVenda(venda1);
+        AvaliaCao avaliacao1 = new AvaliaCao(1, produto1, usuario1,1, "Produto de excelente qualidade");
+        cadastros.CadastraAvaliacao(avaliacao1);
 
-        System.out.println(usuario1.getUsername());
-        System.out.println(produto1.getNome());
-        System.out.println(forma1.getQuantParcelas());
-        System.out.println(forma1.getPercJuros());
+        Consultas consulta1 = new Consultas(cadastros);
+        User usuarioConsultado = consulta1.consultaUsuarioPorId(1);
+        if (usuarioConsultado != null) {
+            System.out.println("Usuário: " + usuarioConsultado.getUsername() + ", ID: " + usuarioConsultado.getId());
+        } else {
+            System.out.println("Usuário não existente");
+        }
+
+        Product produtoConsultado = consulta1.consultaProdutoID(1);
+        if (produtoConsultado != null) {
+            System.out.println("Produto: " + produtoConsultado.getNome() + ", ID: " + produtoConsultado.getId());
+        } else {
+            System.out.println("Produto não existente");
+        }
+
+        Consultas consulta2 = new Consultas(cadastros);
+        consulta2.consultaGeral();
     }
 }
