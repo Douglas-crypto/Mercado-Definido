e10f22d055c75cb14e28b87b298c9dbd27b66c13
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-02-19 14:44:53 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ public class Main {
 
         Cadastros cadastros = new Cadastros();
 
-        User usuario1 = new User(1,"João Silva", "12345678901",
+        User usuario1 = new User(1, "João Silva", "12345678901",
                 "Rua A 123", "joao.silva@email.com", "senha123");
 
         cadastros.CadastroUser(usuario1);
         usuario1.exibirInformacao();
 
-        User usuario2 = new User(1,"João Silva", "12345678901",
-                "Rua A 123", "joao.silva@email.com", "senha123");
+        System.out.println();
+
+        User usuario2 = new User(2, "Maria Oliveiraa", "23456789012",
+                "Rua B, 456", "maria.oliveira@email.com", "senha456");
 
         cadastros.CadastroUser(usuario2);
         usuario2.exibirInformacao();
@@ -21,24 +23,29 @@ public class Main {
 
         FormasPagamento forma1 = new FormasPagamento(1,TipoFormaPagamento.PIX,0,0f);
         cadastros.CadastraFormaPagamento(forma1);
+        forma1.ExibirInformacao();
+        System.out.println();
 
         FormasPagamento forma2 = new FormasPagamento(2,TipoFormaPagamento.CARTAO_CREDITO, 3,15.0f);
         cadastros.CadastraFormaPagamento(forma2);
+        forma2.ExibirInformacao();
+        System.out.println();
 
         FormasPagamento forma3 = new FormasPagamento(3,TipoFormaPagamento.CARTAO_DEBITO, 1,0f);
         cadastros.CadastraFormaPagamento(forma3);
         forma3.ExibirInformacao();
-
         System.out.println();
 
         Product product1 = new Product(1,"Notebook Dell","Notebook Dell Inspiron 15, 8GB RAM, 256GB SSD",
                 3.500f,2,"imagens/notebook_dell.jpg", false);
+
         product1.isUsado(product1);
         cadastros.CadastroProduto(product1);
 
 
         Vendas venda1 = new Vendas(1,usuario1, product1, 1,  forma1);
         cadastros.CadastraVenda(venda1);
+
         venda1.CancelarVenda("Produto estragado");
         venda1.isCancelado();
 
@@ -46,18 +53,27 @@ public class Main {
 
         AvaliaCao avaliacao1 = new AvaliaCao(1, product1, usuario1,1, "Produto de excelente qualidade");
         cadastros.CadastraAvaliacao(avaliacao1);
+        System.out.println(avaliacao1.getId() +
+                avaliacao1.getRating()+
+                avaliacao1.getTexto()+
+                product1.getNome()+
+                usuario1.getUser());
 
-        Consultas consulta1 = new Consultas(cadastros);
+        System.out.println();
+
+      Consultas consulta1 = new Consultas(cadastros);
+
+        consulta1.consultaUsuarioPorId(2);
+
         consulta1.consultaProdutoID(1);
 
+        consulta1.consultaFormasPagamentoID(3);
 
-        Consultas consulta2 = new Consultas(cadastros);
-        consulta2.consultaGeral();
+        consulta1.consultaAvaliaCaoID(1);
 
-        Consultas consulta3 = new Consultas(cadastros);
-        consulta3.consultaUsuarioPorId(1);
+        System.out.println();
 
-        product1.ExibirInformacoes(product1);
+        consulta1.consultaGeral(1,1,2,1,product1);
 
     }
 }
